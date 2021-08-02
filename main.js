b4c3f81e4b1e6f8092ac383e4fe0bbcf3ec9cad6
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2021-08-02 16:13:37 +0600

    Update main.js

--- main.js
+++ main.js
@@ -126,7 +126,7 @@ function draw(){
 	
 	pillarX-=.02;
 	if(pillarX<100-pillarWidth){
-// 		sound('assets/point.mp3')
+		sound('assets/point.mp3')
 		score+=1
 		pillarX=pillarX+450;
 		rand = Math.ceil(Math.random()*(180-10)+10)
